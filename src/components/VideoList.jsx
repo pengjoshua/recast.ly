68a96bac28d50fa4202be06ab023baf3d1d46dab
pengjoshua <joshuapeng7@gmail.com>
2016-11-16 15:32:33 -0800

    Make videoListEntry spec pass

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map(video =>
-      <VideoListEntry video={video} />
+    {props.videos.map((video, i) =>
+      <VideoListEntry video={video} key={i} />
     )}
   </div>
 );
