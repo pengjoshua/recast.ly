953e35c0ba69e1cfe7976962e1108d716b626d20
pengjoshua <joshuapeng7@gmail.com>
2016-11-17 16:48:01 -0800

    Add videoList onMouseEnter/onMouseLeave style changes

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,6 +1,6 @@
 // var ytDurationFormat = require('youtube-duration-format');
 // import as ytDurationFormat from 'youtube-duration-format';
-//got from stack over flow... just styling
+// got from stack overflow... just styling
 var parseDuration = function(e) { 
   var n = e.replace(/D|H|M/g, ':').replace(/P|T|S/g, '').split(':'); 
   if (1 === n.length) {
@@ -14,21 +14,71 @@ var parseDuration = function(e) {
 };
 
 
-var VideoListEntry = (props) => (
+// var VideoListEntry = (props) => (
  
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
-    </div>
-    <div className="media-body">
-      <div onClick={ () => { props.updateVideoPlayer(props.video); } } className="video-list-entry-title">{props.video.snippet.title}</div>
-      {/*<div className="video-list-entry-detail">{props.video.snippet.description}</div>*/}
-      <div className="video-list-entry-detail">{props.video.statistics.viewCount + ' views'
-      + ' - ' + parseDuration(props.video.contentDetails.duration)}</div>
-    </div>
-  </div>
+//   <div className="video-list-entry VLE" onClick={ () => { props.updateVideoPlayer(props.video); } } >
+//     <div className="media-left media-middle">
+//       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+//     </div>
+//     <div className="media-body">
+//       <div className="video-list-entry-title">{props.video.snippet.title}</div>
+//       <div className="video-list-entry-detail VLE">{props.video.snippet.description}</div>
+//       <div className="video-list-entry-detail VLE">{props.video.statistics.viewCount + ' views'
+//       + ' - ' + parseDuration(props.video.contentDetails.duration)}</div>
+//     </div>
+//   </div>
 
-);
+// );
+
+class VideoListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
+
+  onMouseIn() {
+    console.log('hovering!');
+    // event.stopPropagation();
+    this.setState({
+      hover: true
+    });
+  }
+
+  onMouseOut() {
+    console.log('not hovering!');
+    this.setState({
+      hover: false
+    });
+  }
+
+  render() {
+    var style = {
+      color: this.state.hover ? 'white' : '#999',
+      backgroundColor: this.state.hover ? '#e62117' : 'white'
+    };
+
+    var titleStyle = {
+      color: this.state.hover ? 'white' : 'black',
+      backgroundColor: this.state.hover ? '#e62117' : 'white'
+    };
+
+    return (
+      <div style={titleStyle} className="video-list-entry" onMouseEnter={ this.onMouseIn.bind(this) } onMouseLeave={ this.onMouseOut.bind(this) } onClick={ () => { this.props.updateVideoPlayer(this.props.video); } } >
+        <div className="media-left media-middle">
+          <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
+          <div style={style} className="video-list-entry-detail">{this.props.video.snippet.description}</div>
+          <div style={style} className="video-list-entry-detail">{this.props.video.statistics.viewCount + ' views'
+          + ' - ' + parseDuration(this.props.video.contentDetails.duration)}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 // onClick={this.props.updateVideoPlayer(this.props.key)} 
 // PropTypes tell other developers what `props` a component expects
